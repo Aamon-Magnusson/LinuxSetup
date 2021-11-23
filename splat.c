cab1e67dc3cd0d77727be3879f7e8235459b5b60
AMANM6 <aamonm6@gmail.com>
2021-11-22 21:58:40 -0500

    Add files via upload

---
+++ splat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int age = 21;
+	char name[5] = {'\0'};
+
+	printf("Enter your name: ");
+	scanf("%s",name);
+
+	printf("Your name is %s and your age is %d\n", name, age);
+}
