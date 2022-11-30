d351e1a68cec4f768c5c22150645454fa8c92d9a
Aamon Magnusson <aamonm6@gmail.com>
2022-11-29 21:48:26 -0700

    abbr to tmux

--- fish/config.fish
+++ fish/config.fish
@@ -112,6 +112,7 @@ abbr a "sudo apt-get "
 abbr ran "ranger"
 abbr v "nvim"
 alias h "set h (history | fzf --height=50 --border=rounded --prompt='>' --header='Select a command to re-run' --header-first --reverse ); $h"
+abbr t "tmux"
 
 function h
 	set temp (history | fzf)
